3fe0af72b01a6f470f5d20e8abc34682bd8774aa
GwenRaq <gwen.raq@gmail.com>
2016-12-05 08:39:16 -1000

    12/5

---
+++ AandO.js
@@ -0,0 +1,22 @@
+/*
+	Gwyneth Raquepo
+	Arrays and Objects
+	December 5, 2016
+*/
+
+//variables
+
+//arrays
+var numbers = [2,4,6,8,10,12,14,16,18,20];
+var names = ["Senior Tan", "Zakattack", "Yolo Baggins", "Ohai", "Jeff"];
+var colors = ["light black", "ron burgandy", "highlighter"];
+var adj = ["smart", "special case", "yummy", "tiny whiny", "ferocious"];
+var slang = ["scrap", "kobe beef", "da kine", "li 'dat", "pau hana"];
+
+//Objects
+var donut = {
+	toppings: ["sprinkles", "frosting", "chocolate", "ohare air"],
+	filling: "jelly",
+	shape: "square",
+	count: 12
+}
